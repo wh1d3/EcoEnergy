f1aa623d14eaaee34adf4b7cb6c5dcb16f950bd8
Olexander <109623544+wh1d3@users.noreply.github.com>
2024-08-30 19:21:11 +0300

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        
+        public IActionResult About()
+        {
+            return View();
+        }
+        
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
